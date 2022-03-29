85b87944e409a7994e358253f2b8169bf9c8ab58
Agaile Victor <agaile.victor@bolttech.io>
2022-03-29 12:03:19 +0800

    feat - boilerplate code for backemd services

---
+++ NPHC-api/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+var cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 5001;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+module.exports = app;
